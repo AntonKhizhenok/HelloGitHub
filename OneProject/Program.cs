8d38dfdf06d0a7f8ed2244f2f47051114d1c1547
Антон Хиженок <akhizhenok@gmail.com>
2022-06-23 22:39:15 +0300

    added implemetation Weapon

--- OneProject/Program.cs
+++ OneProject/Program.cs
@@ -6,11 +6,81 @@ using System.Threading.Tasks;
 
 namespace OneProject
 {
+    abstract class Weapon
+    {
+        public abstract string Color { get; }
+        public abstract void Recoil();
+
+        public void ShowInfo()
+        {
+         Console.WriteLine(GetType().Name);
+        }
+        public void ItemColor()
+        {
+            Console.WriteLine(Color);
+        }
+    }
+    class Gun : Weapon
+    {
+        public override string Color { get { return "Black"; } }
+
+        public override void Recoil()
+        {
+            
+            Console.WriteLine("Отдача большая");
+        }
+    }
+    class LaserGun : Weapon
+    {
+        public override string Color { get { return "Violet"; } }
+
+        public override void Recoil()
+        {
+           
+            Console.WriteLine("Отдача не большая");
+        }
+    }
+    class Bow : Weapon
+    {
+        public override string Color => "Green";
+
+        public override void Recoil()
+        {
+            
+            Console.WriteLine("Отдача отсутствует");
+        }
+    }
+
+    class Player
+    {
+        public void Otdacha(Weapon weapon)
+        {
+            weapon.Recoil();
+        }
+        public void CheckInfo(Weapon weapon)
+        {
+            weapon.ShowInfo();
+        }
+        public void CheckColor(Weapon weapon)
+        {
+            weapon.ItemColor();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Player player = new Player();
+            Weapon[] weapons = { new Gun(), new LaserGun(), new Bow()};
+            foreach (var item in weapons)
+            {
+                player.CheckInfo(item);
+                player.CheckColor(item);
+                player.Otdacha(item);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
